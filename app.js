08a732528f76d4c39bcc04fa05d5120df5ccc4ba
nikhil7304 <113009805+nikhil7304@users.noreply.github.com>
2024-02-29 13:51:51 +0530

    1st upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function handleClick() {
+    document.querySelector(".mobile").style.display = "flex"
+    document.querySelector(".mobile").style.transition = "4s"
+    
+}
+function handleClose() {
+    document.querySelector(".mobile").style.display = "none"
+    document.querySelector(".mobile").style.transition = "4s"
+
+}
